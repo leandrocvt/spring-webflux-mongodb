d8f7667a2dede585b525700b03582af191681b85
Leandro Cavalcanti <lamaral@brasilseg.com.br>
2023-07-11 11:02:56 -0300

    Feature: - Including webflux and mongodb reactive - Alter repositories

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -1,23 +1,15 @@
 package com.devsuperior.workshopmongo.services;
 
-import java.util.List;
-
+import com.devsuperior.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.devsuperior.workshopmongo.dto.PostDTO;
-import com.devsuperior.workshopmongo.dto.UserDTO;
-import com.devsuperior.workshopmongo.entities.User;
-import com.devsuperior.workshopmongo.repositories.UserRepository;
-import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-
+	/*
 	@Transactional(readOnly = true)
 	public List<UserDTO> findAll() {
 		List<UserDTO> result = repository.findAll().stream().map(x -> new UserDTO(x)).toList();
@@ -65,4 +57,5 @@ public class UserService {
 		entity.setName(dto.getName());
 		entity.setEmail(dto.getEmail());
 	}
+	 */
 }
